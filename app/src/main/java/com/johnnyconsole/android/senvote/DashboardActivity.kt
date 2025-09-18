c73b737071164e934ab40a1aac7f9951af322f76
Johnny Console <johnny.console@hotmail.com>
2025-09-18 11:53:17 -0400

    Create dashboard activity files

---
+++ app/src/main/java/com/johnnyconsole/android/senvote/DashboardActivity.kt
@@ -0,0 +1,28 @@
+package com.johnnyconsole.android.senvote
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.johnnyconsole.android.senvote.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        with(binding) {
+            setContentView(root)
+            ViewCompat.setOnApplyWindowInsetsListener(root, { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+                insets
+            })
+            appBar.activityTitle.text = getString(R.string.activity_title, "Dashboard")
+        }
+    }
+}
