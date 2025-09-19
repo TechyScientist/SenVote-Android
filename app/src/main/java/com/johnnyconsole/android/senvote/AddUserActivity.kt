f52c75f3268e088ac0237d13cfd2522f03f71fc8
Johnny Console <johnny.console@hotmail.com>
2025-09-19 09:50:21 -0400

    Create AddUserActivity files

---
+++ app/src/main/java/com/johnnyconsole/android/senvote/AddUserActivity.kt
@@ -0,0 +1,32 @@
+package com.johnnyconsole.android.senvote
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.johnnyconsole.android.senvote.databinding.ActivityAddUserBinding
+
+class AddUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddUserBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        with(binding) {
+            setContentView(root)
+            ViewCompat.setOnApplyWindowInsetsListener(root) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+                insets
+            }
+            appBar.activityTitle.text = getString(R.string.activity_title, "Add a User")
+
+
+
+
+        }
+    }
+}
